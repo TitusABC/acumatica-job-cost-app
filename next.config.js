da65b0b2bb108805074701c6f7d5f8bf8cf6415d
TitusABC <titus@abontrager.com>
2026-07-19 12:53:38 -0500

    Fix next.config.js: use CommonJS module.exports syntax

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["bcryptjs"],
-  },
+  /* */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
